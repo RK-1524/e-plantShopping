a81de894789c3a51ac62d2be5192b8b35e21d242
RK-1524 <rutujavrpe@gmail.com>
2026-04-29 16:16:11 -0400

    Completed Redux store setup and provider integration

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./CartSlice";
 
+// Create Redux store
 const store = configureStore({
   reducer: {
-    cart: cartReducer
-  }
+    cart: cartReducer,
+  },
 });
 
 export default store;
